3508c7a22faa1c67dfcdda17acd47e0c7589e43e
kvRaghavendra02 <kvraghavendra02@gmail.com>
2023-05-06 17:12:56 +0530

    Dom Intro QuerySelectorALL

--- dom.js
+++ dom.js
@@ -43,13 +43,54 @@
 
 //GETELEMENTBYTAGNAME
 
-var li = document.getElementsByTagName('li');
-console.log(li);
+// var li = document.getElementsByTagName('li');
+// console.log(li);
 
 
-for(var i=0; i<li.length;i++)
+// for(var i=0; i<li.length;i++)
+// {
+//     li[i].style.fontWeight = 'bold';
+//     li[i].style.backgroundColor="#f4f4f4";
+// }
+// li[2].style.backgroundColor="green";
+
+
+//QUERYSELECTOR//
+
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
+
+// var submit = document.querySelector('input[type="submit"]');
+// submit.value="SEND";
+
+// var item = document.querySelector('.list-group-item');
+// item.style.color ='red'
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color ='blue'
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.backgroundColor ='green'
+
+// var thirdItem = document.querySelector('.list-group-item:nth-child(3)');
+// thirdItem.style.display = 'none';
+// or can use thirdItem.hidden = true
+
+
+
+//QUERYSELECTORALL//
+
+var titles =document.querySelectorAll('.list-group-item');
+
+console.log(titles);
+titles[1].style.color='green';
+
+var odd =document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0; i<odd.length; i++)
 {
-    li[i].style.fontWeight = 'bold';
-    li[i].style.backgroundColor="#f4f4f4";
+    odd[i].style.backgroundColor = 'green';
 }
-li[2].style.backgroundColor="green";
