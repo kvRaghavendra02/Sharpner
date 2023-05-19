c7d2d7f06da4b3c2e273b6a3c336f6939ee7224f
kvRaghavendra02 <kvraghavendra02@gmail.com>
2023-05-19 10:24:47 +0530

    Booking app storing the users data as objects DOM

--- bookingApp.js
+++ bookingApp.js
@@ -5,12 +5,26 @@ function submit(e){
     var name = document.getElementById('name').value;
     //console.log(name1);
     var email = document.getElementById('email').value;
-    localStorage.setItem('name', name);
-    localStorage.setItem('email', email);
-    console.log(localStorage.getItem('name'))
-    console.log(localStorage.getItem('email'))
+    // localStorage.setItem('name', name);
+    // localStorage.setItem('email', email);
+    // console.log(localStorage.getItem('name'))
+    // console.log(localStorage.getItem('email'))
+
+    let myObj ={
+        userName: name,
+        userEmail: email
+    };
+
+    //localStorage.setItem("myObj", myObj);// in this we only get object passed that is 2 objects and not the object value because it is not being stored so we need to use JSON
+    //console.log(localStorage);
+
+    let myObj_serialized = JSON.stringify(myObj);// now we have converted object to string
+    localStorage.setItem("myObj", myObj_serialized);// but it is all string and not an object so we need to parse it back to object
+    //console.log(localStorage);
+    let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));//now the string is converted back to the object
+    console.log(myObj_deserialized);
+
 
 }
-var name1 = document.getElementById('name').value;
-//console.log(name1);
+//console.log(name);
 //console.log("hello");
